70413de0de581baea00f5497ae46acbabf971e03
Stayce <send2stayce@gmail.com>
2024-02-27 19:38:21 +0100

    hello template

--- app.py
+++ app.py
@@ -1,7 +1,7 @@
-from flask import Flask, request, jsonify
-import re
 import os
+import re
 import requests
+from flask import (Flask, redirect, render_template, request, jsonify, send_from_directory, url_for)
 
 app = Flask(__name__)
 
@@ -66,6 +66,16 @@ def favicon():
     return send_from_directory(os.path.join(app.root_path, 'static'),
                                'favicon.ico', mimetype='image/vnd.microsoft.icon')
 
+@app.route('/hello', methods=['POST'])
+def hello():
+   name = request.form.get('name')
+
+   if name:
+       print('Request for hello page received with name=%s' % name)
+       return render_template('hello.html', name = name)
+   else:
+       print('Request for hello page received with no name or blank name -- redirecting')
+       return redirect(url_for('index'))
 if __name__ == '__main__':
     app.run(debug=True, port=5000)
 
