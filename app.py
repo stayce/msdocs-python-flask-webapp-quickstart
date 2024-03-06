f596ffbac94c4e6fbbef1af0f982d008a1846a21
Stayce <send2stayce@gmail.com>
2024-03-06 14:43:53 +0100

    weather app and slack bot changes

--- app.py
+++ app.py
@@ -2,45 +2,49 @@ import os
 import re
 import requests
 import datetime
-from flask import (Flask, redirect, render_template, request, jsonify, send_from_directory, url_for)
+from flask import Flask, request
 
 app = Flask(__name__)
 
 # Airtable setup
-AIRTABLE_BASE_ID = 'applUHGDLKLaRM65V'
-AIRTABLE_TABLE_NAME = 'Projects'
+AIRTABLE_BASE_ID = os.getenv('AIRTABLE_BASE_ID')
 
-# Assuming 'AIRTABLE_PERSONAL_ACCESS_TOKEN', 'AIRTABLE_BASE_ID', 'AIRTABLE_TABLE_NAME'
-# are set as App Service settings
-AIRTABLE_PERSONAL_ACCESS_TOKEN = os.getenv('AIRTABLE_PERSONAL_ACCESS_TOKEN')
-# AIRTABLE_BASE_ID = os.getenv('AIRTABLE_BASE_ID')
-# AIRTABLE_TABLE_NAME = os.getenv('AIRTABLE_TABLE_NAME')
-AIRTABLE_ENDPOINT = f'https://api.airtable.com/v0/{AIRTABLE_BASE_ID}/{AIRTABLE_TABLE_NAME}'
+# Slack setup
+SLACK_TOKEN = os.getenv('SLACK_TOKEN')  # Ensure this is set in your environment variables
 
-headers = {
-    'Authorization': f'Bearer {"patj0zu2UFayFZSF1.dfeb1e15ed875fbd349355d7512ec24f875e4c2f7523ac79f9a0f249cd2da321"}',
+# Headers for Airtable
+airtable_headers = lambda token: {
+    'Authorization': f'Bearer {token}',
     'Content-Type': 'application/json',
 }
 
-def post_to_airtable(data):
-    """Post the extracted data to Airtable"""
-    response = requests.post(AIRTABLE_ENDPOINT, json=data, headers=headers)
+# Headers for Slack
+slack_headers = {
+    'Authorization': f'Bearer {SLACK_TOKEN}',
+    'Content-Type': 'application/json',
+}
+
+def post_to_airtable(table_name, data):
+    """Dynamically post the extracted data to the specified Airtable table"""
+    AIRTABLE_PERSONAL_ACCESS_TOKEN = os.getenv('AIRTABLE_PERSONAL_ACCESS_TOKEN')
+    endpoint = f'https://api.airtable.com/v0/{AIRTABLE_BASE_ID}/{table_name}'
+    response = requests.post(endpoint, json=data, headers=airtable_headers(AIRTABLE_PERSONAL_ACCESS_TOKEN))
     print(response.json())
 
-# Function to get username from user ID using Slack API
 def get_username(user_id):
-    response = requests.get(f'https://slack.com/api/users.info?user={user_id}', headers=headers)
+    """Retrieve username from user ID using Slack API"""
+    response = requests.get(f'https://slack.com/api/users.info?user={user_id}', headers=slack_headers)
     user_info = response.json()
-    if response.status_code == 200 and user_info['ok']:
+    if response.status_code == 200 and user_info.get('ok'):
         return user_info['user']['name']
     else:
-        return None
+        return user_id  # Return user ID if unable to fetch username
 
 @app.route('/slack/events', methods=['POST'])
 def slack_events():
     data = request.json
-    
     if data.get('type') == 'event_callback' and data['event'].get('type') == 'message':
+        # Extract necessary information
         message = data['event'].get('text')
         user_id = data['event'].get('user')
         username = get_username(user_id)
@@ -49,36 +53,36 @@ def slack_events():
         channel_id = data['event'].get('channel')
 
         # Retrieve channel information using Slack API
-        response = requests.get(f'https://slack.com/api/conversations.info?channel={channel_id}', headers=headers)
+        response = requests.get(f'https://slack.com/api/conversations.info?channel={channel_id}', headers=slack_headers)
         channel_info = response.json()
-        if response.status_code == 200 and channel_info['ok']:
+        if response.status_code == 200 and channel_info.get('ok'):
             channel_name = channel_info['channel']['name']
-        else:
-            channel_name = "None"
-        
-        # Extract information
-        links = ', '.join(re.findall(r'http[s]?://\S+', message))
-        hashtags = ', '.join(re.findall(r'#\w+', message))
-        text = re.sub(r'(http[s]?://\S+|#\w+)', '', message).strip()
-        
-        # Prepare data for Airtable
-        data_to_post = {
-            "fields": {
-                "Link": links,
-                "Sender": username,
-                "Hashtags": hashtags,
-                "Text": text,
-                "Date": date,
-                "Channel": channel_name
+            # Extract information, adjust regex to account for angle brackets
+            links = ', '.join(re.findall(r'<(http[s]?://\S+)>', message))
+            hashtags = ', '.join(re.findall(r'#\w+', message))
+            text = re.sub(r'(<http[s]?://\S+>|#\w+)', '', message).strip()
+            
+            # Prepare data for Airtable
+            data_to_post = {
+                "fields": {
+                    "Link": links,
+                    "Sender": username,
+                    "Hashtags": hashtags,
+                    "Text": text,
+                    "Date": date,
+                    "Channel": channel_name
+                }
             }
-        }
-        
-        # Post to Airtable
-        post_to_airtable(data_to_post)
+            
+            # Check if the channel is one of the specified ones before posting
+            if channel_name.lower() in ['projects', 'research']:  # Add more channel names as needed
+                post_to_airtable(channel_name, data_to_post)
         
         return 'OK', 200
     
     return 'OK', 200
+
+
 
 @app.route('/')
 def index():
